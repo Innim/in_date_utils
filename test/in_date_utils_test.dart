04e248d3878bc62ed0ea565aa47c92c7616e5773
greymag <greymag@gmail.com>
2020-11-17 17:55:44 +0300

    Refactor tests in groups.

--- test/in_date_utils_test.dart
+++ test/in_date_utils_test.dart
@@ -2,143 +2,180 @@ import 'package:in_date_utils/in_date_utils.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('startOfDay should return correct date', () {
-    final date = DateTime(2019, 12, 3, 18, 10, 30);
-    final res = DateUtils.startOfDay(date);
+  group('startOfDay()', () {
+    test('should return correct date', () {
+      final date = DateTime(2019, 12, 3, 18, 10, 30);
+      final res = DateUtils.startOfDay(date);
 
-    expect(res, DateTime(2019, 12, 3));
+      expect(res, DateTime(2019, 12, 3));
+    });
   });
 
-  test('startOfNextDay should return correct date', () {
-    final date = DateTime(2019, 12, 3, 18, 10, 30);
-    final res = DateUtils.startOfNextDay(date);
+  group('startOfNextDay()', () {
+    test('should return correct date', () {
+      final date = DateTime(2019, 12, 3, 18, 10, 30);
+      final res = DateUtils.startOfNextDay(date);
 
-    expect(res, DateTime(2019, 12, 4));
+      expect(res, DateTime(2019, 12, 4));
+    });
   });
 
-  test('startOfToday should return correct date', () {
-    final date = DateTime.now();
-    final res = DateUtils.startOfToday();
+  group('startOfToday()', () {
+    test('should return correct date', () {
+      final date = DateTime.now();
+      final res = DateUtils.startOfToday();
 
-    expect(res, DateTime(date.year, date.month, date.day));
+      expect(res, DateTime(date.year, date.month, date.day));
+    });
   });
 
-  test('setTime should change time', () {
-    final date = DateTime(2019, 12, 3, 18, 10, 30);
+  group('setTime()', () {
+    test('should change time', () {
+      final date = DateTime(2019, 12, 3, 18, 10, 30);
 
-    expect(DateUtils.setTime(date, 12, 10).hour, 12);
-    expect(DateUtils.setTime(date, 12, 10).second, 0);
-    expect(DateUtils.setTime(date, 12, 10, 30).minute, 10);
-    expect(DateUtils.setTime(date, 12, 10, 30).second, 30);
-    expect(DateUtils.setTime(date, 12, 10, 30).millisecond, 0);
-    expect(DateUtils.setTime(date, 12, 10, 30, 13).millisecond, 13);
-    expect(DateUtils.setTime(date, 12, 10, 30).microsecond, 0);
-    expect(DateUtils.setTime(date, 12, 10, 30, 13).microsecond, 0);
-    expect(DateUtils.setTime(date, 12, 10, 30, 13, 66).microsecond, 66);
+      expect(DateUtils.setTime(date, 12, 10).hour, 12);
+      expect(DateUtils.setTime(date, 12, 10).second, 0);
+      expect(DateUtils.setTime(date, 12, 10, 30).minute, 10);
+      expect(DateUtils.setTime(date, 12, 10, 30).second, 30);
+      expect(DateUtils.setTime(date, 12, 10, 30).millisecond, 0);
+      expect(DateUtils.setTime(date, 12, 10, 30, 13).millisecond, 13);
+      expect(DateUtils.setTime(date, 12, 10, 30).microsecond, 0);
+      expect(DateUtils.setTime(date, 12, 10, 30, 13).microsecond, 0);
+      expect(DateUtils.setTime(date, 12, 10, 30, 13, 66).microsecond, 66);
+    });
   });
 
-  test('copyWith should replace only passed values', () {
-    final date = DateTime(2019, 12, 3, 18, 10, 30, 55, 66);
+  group('copyWith()', () {
+    test('should replace only passed values', () {
+      final date = DateTime(2019, 12, 3, 18, 10, 30, 55, 66);
 
-    expect(DateUtils.copyWith(date, year: 2020),
-        DateTime(2020, 12, 3, 18, 10, 30, 55, 66));
-    expect(DateUtils.copyWith(date, month: 9),
-        DateTime(2019, 9, 3, 18, 10, 30, 55, 66));
-    expect(DateUtils.copyWith(date, day: 12),
-        DateTime(2019, 12, 12, 18, 10, 30, 55, 66));
-    expect(DateUtils.copyWith(date, hour: 23),
-        DateTime(2019, 12, 3, 23, 10, 30, 55, 66));
-    expect(DateUtils.copyWith(date, minute: 32),
-        DateTime(2019, 12, 3, 18, 32, 30, 55, 66));
-    expect(DateUtils.copyWith(date, second: 44),
-        DateTime(2019, 12, 3, 18, 10, 44, 55, 66));
-    expect(DateUtils.copyWith(date, millisecond: 99),
-        DateTime(2019, 12, 3, 18, 10, 30, 99, 66));
-    expect(DateUtils.copyWith(date, microsecond: 43),
-        DateTime(2019, 12, 3, 18, 10, 30, 55, 43));
-    expect(DateUtils.copyWith(date, year: 2018, month: 4, hour: 10),
-        DateTime(2018, 4, 3, 10, 10, 30, 55, 66));
+      expect(DateUtils.copyWith(date, year: 2020),
+          DateTime(2020, 12, 3, 18, 10, 30, 55, 66));
+      expect(DateUtils.copyWith(date, month: 9),
+          DateTime(2019, 9, 3, 18, 10, 30, 55, 66));
+      expect(DateUtils.copyWith(date, day: 12),
+          DateTime(2019, 12, 12, 18, 10, 30, 55, 66));
+      expect(DateUtils.copyWith(date, hour: 23),
+          DateTime(2019, 12, 3, 23, 10, 30, 55, 66));
+      expect(DateUtils.copyWith(date, minute: 32),
+          DateTime(2019, 12, 3, 18, 32, 30, 55, 66));
+      expect(DateUtils.copyWith(date, second: 44),
+          DateTime(2019, 12, 3, 18, 10, 44, 55, 66));
+      expect(DateUtils.copyWith(date, millisecond: 99),
+          DateTime(2019, 12, 3, 18, 10, 30, 99, 66));
+      expect(DateUtils.copyWith(date, microsecond: 43),
+          DateTime(2019, 12, 3, 18, 10, 30, 55, 43));
+      expect(DateUtils.copyWith(date, year: 2018, month: 4, hour: 10),
+          DateTime(2018, 4, 3, 10, 10, 30, 55, 66));
+    });
   });
 
-  test('nextMonth should return correct month number', () {
-    expect(DateUtils.nextMonth(DateTime(2019, 1)), 2);
-    expect(DateUtils.nextMonth(DateTime(2019, 12)), 1);
-    expect(DateUtils.nextMonth(DateTime(2019, 13)), 2);
-    expect(DateUtils.nextMonth(DateTime(2018, 6)), 7);
+  group('nextMonth()', () {
+    test('nextMonth should return correct month number', () {
+      expect(DateUtils.nextMonth(DateTime(2019, 1)), 2);
+      expect(DateUtils.nextMonth(DateTime(2019, 12)), 1);
+      expect(DateUtils.nextMonth(DateTime(2019, 13)), 2);
+      expect(DateUtils.nextMonth(DateTime(2018, 6)), 7);
+    });
   });
 
-  test('firstDayOfWeek should return correct date time', () {
-    expect(DateUtils.firstDayOfWeek(DateTime(2019, 1, 6, 23, 59, 59, 999, 999)),
-        DateTime(2018, 12, 31, 0, 0, 0, 0, 0));
-    expect(DateUtils.firstDayOfWeek(DateTime(2020, 4, 9, 15)),
-        DateTime(2020, 4, 6, 0, 0, 0, 0, 0));
-    expect(
-        DateUtils.firstDayOfWeek(DateTime(2020, 12, 27, 23, 59, 59, 999, 999)),
-        DateTime(2020, 12, 21, 0, 0, 0, 0, 0));
+  group('firstDayOfWeek()', () {
+    test('should return correct date time', () {
+      expect(
+          DateUtils.firstDayOfWeek(DateTime(2019, 1, 6, 23, 59, 59, 999, 999)),
+          DateTime(2018, 12, 31, 0, 0, 0, 0, 0));
+      expect(DateUtils.firstDayOfWeek(DateTime(2020, 4, 9, 15)),
+          DateTime(2020, 4, 6, 0, 0, 0, 0, 0));
+      expect(
+          DateUtils.firstDayOfWeek(
+              DateTime(2020, 12, 27, 23, 59, 59, 999, 999)),
+          DateTime(2020, 12, 21, 0, 0, 0, 0, 0));
+    });
   });
 
-  test('firstDayOfNextWeek should return correct date time', () {
-    expect(DateUtils.firstDayOfNextWeek(DateTime(2018, 12, 31, 0, 0, 0, 0, 0)),
-        DateTime(2019, 1, 07, 0, 0, 0, 0, 0));
-    expect(DateUtils.firstDayOfNextWeek(DateTime(2020, 4, 9, 15)),
-        DateTime(2020, 4, 13, 0, 0, 0, 0, 0));
-    expect(
-        DateUtils.firstDayOfNextWeek(
-            DateTime(2020, 4, 12, 23, 59, 59, 999, 999)),
-        DateTime(2020, 4, 13, 0, 0, 0, 0, 0));
+  group('firstDayOfNextWeek()', () {
+    test('should return correct date time', () {
+      expect(
+          DateUtils.firstDayOfNextWeek(DateTime(2018, 12, 31, 0, 0, 0, 0, 0)),
+          DateTime(2019, 1, 07, 0, 0, 0, 0, 0));
+      expect(DateUtils.firstDayOfNextWeek(DateTime(2020, 4, 9, 15)),
+          DateTime(2020, 4, 13, 0, 0, 0, 0, 0));
+      expect(
+          DateUtils.firstDayOfNextWeek(
+              DateTime(2020, 4, 12, 23, 59, 59, 999, 999)),
+          DateTime(2020, 4, 13, 0, 0, 0, 0, 0));
+    });
   });
 
-  test('lastDayOfWeek should return correct date time', () {
-    expect(DateUtils.lastDayOfWeek(DateTime(2018, 12, 31, 0, 0, 0, 0, 0)),
-        DateTime(2019, 1, 06, 0, 0, 0, 0, 0));
-    expect(DateUtils.lastDayOfWeek(DateTime(2020, 4, 9, 15)),
-        DateTime(2020, 4, 12, 0, 0, 0, 0, 0));
-    expect(DateUtils.lastDayOfWeek(DateTime(2020, 4, 12, 23, 59, 59, 999, 999)),
-        DateTime(2020, 4, 12, 0, 0, 0, 0, 0));
+  group('lastDayOfWeek()', () {
+    test('should return correct date time', () {
+      expect(DateUtils.lastDayOfWeek(DateTime(2018, 12, 31, 0, 0, 0, 0, 0)),
+          DateTime(2019, 1, 06, 0, 0, 0, 0, 0));
+      expect(DateUtils.lastDayOfWeek(DateTime(2020, 4, 9, 15)),
+          DateTime(2020, 4, 12, 0, 0, 0, 0, 0));
+      expect(
+          DateUtils.lastDayOfWeek(DateTime(2020, 4, 12, 23, 59, 59, 999, 999)),
+          DateTime(2020, 4, 12, 0, 0, 0, 0, 0));
+    });
   });
 
-  test('firstDayOfMonth should return correct date time', () {
-    expect(
-        DateUtils.firstDayOfMonth(DateTime(2019, 1, 6, 23, 59, 59, 999, 999)),
-        DateTime(2019, 1, 1, 0, 0, 0, 0, 0));
-    expect(DateUtils.firstDayOfMonth(DateTime(2020, 4, 9, 15, 16)),
-        DateTime(2020, 4, 1, 0, 0, 0, 0, 0));
-    expect(
-        DateUtils.firstDayOfMonth(DateTime(2020, 12, 27, 23, 59, 59, 999, 999)),
-        DateTime(2020, 12, 1, 0, 0, 0, 0, 0));
+  group('firstDayOfMonth()', () {
+    test('should return correct date time', () {
+      expect(
+          DateUtils.firstDayOfMonth(DateTime(2019, 1, 6, 23, 59, 59, 999, 999)),
+          DateTime(2019, 1, 1, 0, 0, 0, 0, 0));
+      expect(DateUtils.firstDayOfMonth(DateTime(2020, 4, 9, 15, 16)),
+          DateTime(2020, 4, 1, 0, 0, 0, 0, 0));
+      expect(
+          DateUtils.firstDayOfMonth(
+              DateTime(2020, 12, 27, 23, 59, 59, 999, 999)),
+          DateTime(2020, 12, 1, 0, 0, 0, 0, 0));
+    });
   });
 
-  test('firstDayOfNextMonth should return correct date time', () {
-    expect(DateUtils.firstDayOfNextMonth(DateTime(2020, 4, 9, 15, 16)),
-        DateTime(2020, 5, 1, 0, 0, 0, 0, 0));
+  group('firstDayOfNextMonth()', () {
+    test('should return correct date time', () {
+      expect(DateUtils.firstDayOfNextMonth(DateTime(2020, 4, 9, 15, 16)),
+          DateTime(2020, 5, 1, 0, 0, 0, 0, 0));
+    });
   });
 
-  test('lastDayOfMonth should return correct date time', () {
-    expect(DateUtils.lastDayOfMonth(DateTime(2019, 1, 6, 23, 59, 59, 999, 999)),
-        DateTime(2019, 1, 31, 0, 0, 0, 0, 0));
-    expect(DateUtils.lastDayOfMonth(DateTime(2020, 4, 9, 15, 16)),
-        DateTime(2020, 4, 30, 0, 0, 0, 0, 0));
-    expect(
-        DateUtils.lastDayOfMonth(DateTime(2020, 12, 27, 23, 59, 59, 999, 999)),
-        DateTime(2020, 12, 31, 0, 0, 0, 0, 0));
+  group('lastDayOfMonth()', () {
+    test('should return correct date time', () {
+      expect(
+          DateUtils.lastDayOfMonth(DateTime(2019, 1, 6, 23, 59, 59, 999, 999)),
+          DateTime(2019, 1, 31, 0, 0, 0, 0, 0));
+      expect(DateUtils.lastDayOfMonth(DateTime(2020, 4, 9, 15, 16)),
+          DateTime(2020, 4, 30, 0, 0, 0, 0, 0));
+      expect(
+          DateUtils.lastDayOfMonth(
+              DateTime(2020, 12, 27, 23, 59, 59, 999, 999)),
+          DateTime(2020, 12, 31, 0, 0, 0, 0, 0));
+    });
   });
 
-  test('firstDayOfYear should return correct date time', () {
-    expect(DateUtils.firstDayOfYear(DateTime(2020, 4, 9, 15, 16)),
-        DateTime(2020, 1, 1, 0, 0, 0, 0, 0));
+  group('firstDayOfYear()', () {
+    test('should return correct date time', () {
+      expect(DateUtils.firstDayOfYear(DateTime(2020, 4, 9, 15, 16)),
+          DateTime(2020, 1, 1, 0, 0, 0, 0, 0));
+    });
   });
 
-  test('firstDayOfNextYear should return correct date time', () {
-    expect(DateUtils.firstDayOfNextYear(DateTime(2020, 4, 9, 15, 16)),
-        DateTime(2021, 1, 1, 0, 0, 0, 0, 0));
+  group('firstDayOfNextYear()', () {
+    test('should return correct date time', () {
+      expect(DateUtils.firstDayOfNextYear(DateTime(2020, 4, 9, 15, 16)),
+          DateTime(2021, 1, 1, 0, 0, 0, 0, 0));
+    });
   });
 
-  test('lastDayOfYear should return correct date time', () {
-    expect(DateUtils.lastDayOfYear(DateTime(2020, 4, 9, 15, 16)),
-        DateTime(2020, 12, 31, 0, 0, 0, 0, 0));
+  group('lastDayOfYear()', () {
+    test('should return correct date time', () {
+      expect(DateUtils.lastDayOfYear(DateTime(2020, 4, 9, 15, 16)),
+          DateTime(2020, 12, 31, 0, 0, 0, 0, 0));
+    });
   });
 
-  group('isSameDay', () {
+  group('isSameDay()', () {
     test('should return true if in the same day', () {
       final d1 = DateTime(2020, 10, 26, 18, 02, 59);
       final d2 = DateTime(2020, 10, 26, 10, 02, 59);
@@ -162,7 +199,7 @@ void main() {
     });
   });
 
-  group('generateWithDayStep', () {
+  group('generateWithDayStep()', () {
     test('should include start date and exclude end', () {
       final start = DateTime(2020, 11, 02, 18, 7);
       final end = DateTime(2020, 11, 03, 17, 3);
